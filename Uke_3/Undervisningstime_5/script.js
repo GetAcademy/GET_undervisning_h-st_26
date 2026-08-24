e2b8b3c8ef305bb68b57cd3a038638f02b88d4e8
Rebecka Kinn <rebeckaki@getacademy.no>
2026-08-24 10:45:08 +0200

    adding structure for next lesson

---
+++ Uke_3/Undervisningstime_5/script.js
@@ -0,0 +1,52 @@
+let points = 0;
+let clicks = 0;
+let amountToAdd = 1;
+let upgradesBought = 0;
+let upgradePrice1 = 10;
+let upgradePrice2 = 20;
+
+function addPoints() {
+  points += amountToAdd;
+  clicks++;
+  updateDisplay();
+}
+
+function updateDisplay() {
+  document.getElementById("points").innerHTML = points;
+  document.getElementById("clicks").innerHTML = clicks;
+  document.getElementById("upgradesBought").innerHTML = upgradesBought;
+  document.getElementById("upgrade_1_price").innerHTML = upgradePrice1;
+  document.getElementById("upgrade_2_price").innerHTML = upgradePrice2;
+  checkUpgradeButtons();
+}
+
+function addUpgrade(amount, upgradeButton) {
+  let buttonId = upgradeButton.id;
+  console.log(buttonId);
+  upgradesBought++;
+  amountToAdd += amount;
+  if (buttonId == "upgrade_1") {
+    points -= upgradePrice1;
+    upgradePrice1 = upgradePrice1 * 2;
+  } else if (buttonId == "upgrade_2") {
+    points -= upgradePrice2;
+    upgradePrice2 = upgradePrice2 * 2;
+  } else {
+    console.log("Feil upgrade-knapp!");
+  }
+  updateDisplay();
+}
+
+function checkUpgradeButtons() {
+  if (points > upgradePrice1) {
+    document.getElementById("upgrade_1").disabled = false;
+  } else {
+    document.getElementById("upgrade_1").disabled = true;
+  }
+
+  if (points > upgradePrice2) {
+    document.getElementById("upgrade_2").disabled = false;
+  } else {
+    document.getElementById("upgrade_2").disabled = true;
+  }
+}
