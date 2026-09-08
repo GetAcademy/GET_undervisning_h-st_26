85cca2d73bbd7877055e5f74321e29ab885bef22
Rebecka Kinn <rebeckaki@getacademy.no>
2026-09-08 09:40:58 +0200

    added files and default css

---
+++ Uke_5/Undervisningstime_10/ToDo-app/script.js
@@ -0,0 +1,12 @@
+//Model
+const app = document.getElementById("app");
+
+//View
+updateView();
+function updateView() {
+  app.innerHTML = /*HTML*/ `
+        <h1>ToDo</h1>
+    `;
+}
+
+//Controller
